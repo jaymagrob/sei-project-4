deba24798068e40ff3520b9d715435829fa3d5b2
jaymagrob <johnmagnusrobertson@gmail.com>
2020-03-05 12:03:21 +0000

    Ready for deployment

---
+++ frontend/build/precache-manifest.d2d7fbc50c809b81e43f7e8fad1c33e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cdf2b78445fab505b5d574e861266fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05f67aee7e3ca040e2c6",
+    "url": "/static/css/2.7c937a1c.chunk.css"
+  },
+  {
+    "revision": "ff0133e437ca53f5bcd1",
+    "url": "/static/css/main.9e06d04d.chunk.css"
+  },
+  {
+    "revision": "05f67aee7e3ca040e2c6",
+    "url": "/static/js/2.66a8cf27.chunk.js"
+  },
+  {
+    "revision": "ca2529ec811b23fbaecc956862a512b2",
+    "url": "/static/js/2.66a8cf27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0133e437ca53f5bcd1",
+    "url": "/static/js/main.8816c7da.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "2d6807005bbaaaec1d7e7befc2ee333d",
+    "url": "/static/media/Skarpa_Logo.2d680700.png"
+  }
+]);
--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import index
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('api/', include('jwt_auth.urls')),
     path('api/boards/', include('boards.urls')),
